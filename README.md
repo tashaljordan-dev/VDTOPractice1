1bb96e2738e742ccd920f17897b285ca18f597b3
tashaljordan-dev <tashaljordan@gmail.com>
2025-09-13 16:32:16 +0000

    practice1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VDTOPractice1
 Practice 1: Code Correction
+let name = "Alice";
+let itemNumber = 5;
+let itemCost = 20;
+let output = name + " bought " + itemNumber + " items for $" + itemCost + ".";
+console.log(output);
---
+++ app.js
@@ -0,0 +1,7 @@
+// Simple Node.js app
+console.log('Hello from Node.js!');
+let personName = "Alice";
+let itemNumber = 5;
+let itemCost = 20;
+let output = personName + " bought " + itemNumber + " items for $" + itemCost + ".";
+console.log(output);
