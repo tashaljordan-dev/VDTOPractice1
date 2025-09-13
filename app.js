2ff1354dc8053d822a9c382f24f73f5976a4cd4b
tashaljordan-dev <tashaljordan@gmail.com>
2025-09-13 11:46:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ let itemNumber = 5;
 let itemCost = 20;
 let output = personName + " bought " + itemNumber + " items for $" + itemCost + ".";
 console.log(output);
-let totalCost = itemNumber ** itemCost;
+let totalCost = personName + " spent " + itemNumber * itemCost + ".";
 console.log(totalCost);
